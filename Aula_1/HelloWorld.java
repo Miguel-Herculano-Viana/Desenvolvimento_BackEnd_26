5bf6374101323c13c515d3b97520086ef304f834
Miguel-Herculano-Viana <miguel.herculanoviana@gmail.com>
2026-08-03 23:00:21 -0300

    Aula 01 - exemplo java

---
+++ Aula_1/HelloWorld.java
@@ -0,0 +1,8 @@
+package Aula_1;
+
+public class HelloWorld {
+    //Cria uma função
+    public static void main(String[] args){
+        System.out.println("Hello World!"); //imprime informações no terminal
+    }   
+}
